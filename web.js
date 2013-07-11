c30c05705ff5e82ccf445930d35ce70ba71c75e2
Mike Breecher <michaelbreecher@gmail.com>
2013-07-11 04:23:13 +0000

    update required variable syntax

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs = require('fs');
+var fs = require('fs');
 var output = fs.readFileSync("./index.html", "utf8");
 
 app.get('/', function(request, response) {
